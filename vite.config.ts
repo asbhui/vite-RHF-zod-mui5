932773433bebc63d014231a108803b188b060efc
Arvinder Singh <asbhui@gmail.com>
2024-02-03 20:11:45 +0000

    fix: linting and prettify the code

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
