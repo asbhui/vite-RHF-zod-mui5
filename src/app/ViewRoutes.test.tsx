faa39deb293ee7b16716b9fceb2b1b552717b94f
Arvinder Singh <asbhui@gmail.com>
2024-02-05 23:42:52 +0000

    test: working with two different examples

--- src/app/ViewRoutes.test.tsx
+++ src/app/ViewRoutes.test.tsx
@@ -1,7 +1,7 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import { ViewRoutes, WrappedViewRoutes } from './ViewRoutes';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import { WrappedViewRoutes, routesFromElements } from './ViewRoutes';
 
 describe('App', () => {
   it('Renders hello world', () => {
@@ -15,16 +15,29 @@ describe('App', () => {
       }),
     ).toHaveTextContent('Home');
   });
-  it('Renders not found if invalid path', () => {
-    render(
-      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
-        <ViewRoutes />
-      </MemoryRouter>,
-    );
-    expect(
-      screen.getByRole('heading', {
-        level: 1,
-      }),
-    ).toHaveTextContent('404');
+
+  // it('Renders not found if invalid path', () => {
+  //   render(
+  //     <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+  //       <ViewRoutes />
+  //     </MemoryRouter>,
+  //   );
+  //   expect(
+  //     screen.getByRole('heading', {
+  //       level: 1,
+  //     }),
+  //   ).toHaveTextContent('404');
+  // });
+
+  it('Renders not found if invalid path', async () => {
+    const inMemoryRouter = createMemoryRouter(routesFromElements, {
+      initialEntries: ['/', '/this-route-does-not-exist'],
+      initialIndex: 1,
+    });
+
+    render(<RouterProvider router={inMemoryRouter} />);
+
+    expect(await screen.findByRole('heading', { level: 1 })).toHaveTextContent('404');
+    expect(screen.getByText('Page not found')).toBeInTheDocument();
   });
 });
--- src/app/ViewRoutes.tsx
+++ src/app/ViewRoutes.tsx
@@ -1,9 +1,9 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom';
 import { NotFound } from '../shared/ui/NotFound';
 import { Home } from '../shared/ui/Home';
 import { Typos } from '../shared/ui/Typos';
 
-export const ViewRoutes = () => {
+/* export const ViewRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -14,7 +14,18 @@ export const ViewRoutes = () => {
 };
 
 export const WrappedViewRoutes = () => (
-  <HashRouter>
+  <BrowserRouter>
     <ViewRoutes />
-  </HashRouter>
+  </BrowserRouter>
 );
+ */
+
+export const routesFromElements = createRoutesFromElements(
+  <Route path="/">
+    <Route index element={<Home />} />
+    <Route path="typos" element={<Typos />} />
+    <Route path="*" element={<NotFound />} />
+  </Route>,
+);
+
+export const WrappedViewRoutes = () => <RouterProvider router={createBrowserRouter(routesFromElements)} />;
