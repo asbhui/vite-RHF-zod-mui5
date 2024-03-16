a4127d4c5464eea225487a479759c5e0d1f8bd4b
Arvinder Singh <74012669+asingh74@users.noreply.github.com>
2024-03-16 16:57:01 +0000

    feat: example for Autocomplete having label (#9)

--- src/app/ViewRoutes.tsx
+++ src/app/ViewRoutes.tsx
@@ -1,6 +1,8 @@
 import { lazy } from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
+import ComboBox from '../shared/components/ComboBox';
+
 const Root = lazy(() => import('../shared/ui/Root').then((module) => ({ default: module.Root })));
 const Home = lazy(() => import('../shared/ui/Home').then((module) => ({ default: module.Home })));
 const Typos = lazy(() => import('../shared/ui/Typos').then((module) => ({ default: module.Typos })));
@@ -10,6 +12,7 @@ export const routesFromElements = createRoutesFromElements(
   <Route path="/" element={<Root />}>
     <Route index element={<Home />} />
     <Route path="typos" element={<Typos />} />
+    <Route path="comboBox" element={<ComboBox />} />
     <Route path="*" element={<NotFound />} />
   </Route>,
 );
