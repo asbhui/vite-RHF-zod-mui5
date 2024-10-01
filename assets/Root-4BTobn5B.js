90767884a650e98614f142667b829e99ff33b574
asbhui <asbhui@users.noreply.github.com>
2024-10-01 14:48:45 +0000

    deploy: ad370d8ab7f5378bc0eba2ee3f3a2fabff11ea3d

---
+++ assets/Root-4BTobn5B.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-CCjMSFS0.js";import{C as n}from"./Container-HV0ZPLvE.js";const i=()=>t.jsx(n,{sx:{height:"100vh",bgcolor:"#f5f5f5",display:"flex",flexDirection:"column",justifyContent:"center",alignContent:"center"},children:t.jsx("main",{children:t.jsx(e,{})})});export{i as Root};
