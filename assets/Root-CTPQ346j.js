3fbf5c2ab5c1d957560199c78a15450827cad01c
Arvinder Singh <asbhui@gmail.com>
2024-10-01 15:16:31 +0100

    Updates

---
+++ assets/Root-CTPQ346j.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-LbJi6_LS.js";import{C as n}from"./Container-BEAwUYaL.js";const i=()=>t.jsx(n,{sx:{height:"100vh",bgcolor:"#f5f5f5",display:"flex",flexDirection:"column",justifyContent:"center",alignContent:"center"},children:t.jsx("main",{children:t.jsx(e,{})})});export{i as Root};
